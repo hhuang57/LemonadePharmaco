9ad101b2bb34ca2ef391b2705a5c3728eac61558
Barbara <barbara.h.kim@gmail.com>
2016-05-05 04:38:37 -0400

    Multidosing

--- Tenofovir.m
+++ Tenofovir.m
@@ -1,4 +1,4 @@
-function [outAUC,outBalance,outT,outY] = Tenofovir(p,p_viral,OutputVar,TimeLen)
+function [outAUC,outBalance,outT,outY] = Tenofovir(p,p_viral,y0_viral,OutputVar,TimeLen)
 
 mtfv = 287.2; %Molecular weight of tenofovir (g/mol)
 mmp = 367.2; %Molecular weight of tenofovir monophosphate
@@ -11,15 +11,12 @@ Vbl = p(22);
 N = p(23); %PBMC per L of blood
 Vcell2 = Vcell*N*Vbl;
 
-% y0 = [0 0 0 0 0 D0/V1 0]'; % moles/L
-y0 = [0 0 0 0 0 D0 0]'; % moles 
-y0_viral = set_initial_conditions(400);
+y0 = [0 0 0 0 0 D0 0]'; % moles
 y0 = [y0; y0_viral'];
 p = [p; p_viral];
 options = odeset('MaxStep',5e-2, 'AbsTol', 1e-5,'RelTol', 1e-5,'InitialStep', 1e-2);
 % [T1,Y1] = ode45(@Tenofovir_eqns,[0 TimeLen],y0,options,p);
-[T1,Y1] = ode23s(@Tenofovir_eqns,[0 TimeLen],y0,options,p);
-
+[T1,Y1] = ode23s(@Tenofovir_eqns,[0 24],y0,options,p);
 DrugIn = ones(size(T1))*D0; % cumulative drug into system
 TotalFreeD(:,1) = Y1(:,1);
 TotalFreeD(:,2) = Y1(:,2);
@@ -29,13 +26,31 @@ TotalDdp = Y1(:,5);
 TotalProD = Y1(:,6);
 DrugOut = Y1(:,7); % cumulative drug eliminated from system
 BalanceD1 = DrugIn - DrugOut - TotalProD - TotalFreeD(:,1) - TotalFreeD(:,2) - TotalFreeD(:,3) - TotalDmp - TotalDdp; %(zero = balance)
-
+clearvars TotalFreeD;
+for T=24:24:TimeLen
+    y0 = Y1(end,:);
+    y0(6) = y0(6) + D0;
+    [t,y] = ode23s(@Tenofovir_eqns,[0 24],y0,options,p);
+    DrugIn = ones(size(t))*(T/24 + 1)*D0; % cumulative drug into system
+    TotalFreeD(:,1) = y(:,1);
+    TotalFreeD(:,2) = y(:,2);
+    TotalFreeD(:,3) = y(:,3);
+    TotalDmp = y(:,4);
+    TotalDdp = y(:,5);
+    TotalProD = y(:,6);
+    DrugOut = y(:,7); % cumulative drug eliminated from system
+    balance = DrugIn - DrugOut - TotalProD - TotalFreeD(:,1) - TotalFreeD(:,2) - TotalFreeD(:,3) - TotalDmp - TotalDdp; %(zero = balance)
+    BalanceD1 = [BalanceD1; balance];
+    T1 = [T1; t+T];
+    Y1 = [Y1; y];
+    clearvars TotalFreeD;
+end
 % Include a check on the molecular balance. Don't want to do it visually
-% for 48,000 runs! Instead define a criterion. For example, alert us for 
+% for 48,000 runs! Instead define a criterion. For example, alert us for
 % any mismatch by more than 1 molecule in a million (10^-6)
 check = max(max(BalanceD1))/(D0/V1);
 % fprintf ('Molecular Balance = %2.1e\n',check);
-if check > 1.e-6
+if check > 1.e-5
     fprintf ('*** Molecular Balance Violated ***\n');
 end
 
--- driver_Lemonade.m
+++ driver_Lemonade.m
@@ -2,10 +2,10 @@
 clear all;
 close all;
 
+
 %% Initialize PK parameters
 mtfv = 287.2; %Molecular weight of tenofovir (g/mol)
 
-dose = 300*10^6; %dose in ng, taken orally
 V1= 244; %Volume of central compartment (L)
 V2= 464.54; %Volume of peripheral compartment (L)
 Vcell= 0.28*10^-12; %Volume of PBMC compartment
@@ -32,7 +32,7 @@ N = 0.9*10^6*10^3; %PBMC per ml of blood (cells/L)
 % kleak = 9.2*10^3; % Leakage of TFV
 kleak = 3.4*10^10; % Leakage of TFV
 Vcell2 = Vcell*N*Vbl;
-p = [dose V1 V2 Vcell Fbio ka Q Cl kout kcat1 kcat2 Km1 Km2 Km Vmax E1 E2 GSHi GSHe fu F Vbl N kleak]';
+p = [0 V1 V2 Vcell Fbio ka Q Cl kout kcat1 kcat2 Km1 Km2 Km Vmax E1 E2 GSHi GSHe fu F Vbl N kleak]';
 
 %% Initialize PD parameters: Viral Dynamics
 gammaT=2e9; % birth rate of uninfected T-cells, 1/day
@@ -40,17 +40,17 @@ gammaM=6.9e7; % birth rate of uninfected macrophages, 1/dzy
 deltaT=0.02; % death rate of uninfected T-cells, 1/day
 deltaM=0.0069; % death rate of uninfected macrophages
 
-% intracellular degradation of essential components of the pre-integration 
-% complex, e.g., by the host cell proteasome, which return early infected 
+% intracellular degradation of essential components of the pre-integration
+% complex, e.g., by the host cell proteasome, which return early infected
 % T-cells and macrophages to an uninfected stage, respectively
 deltaPICT=0.35;
 deltaPICM=0.0035;
 
 % rate constants of proviral integration into the host cell’s genome
 kT=0.35;
-kM=0.07; 
+kM=0.07;
 
-% total number of released infectious and non-infectious 
+% total number of released infectious and non-infectious
 NhatT=1000;
 NhatM=100;
 % virus from late infected T-cells and macrophages
@@ -61,59 +61,70 @@ NM=0.67*NhatM;
 deltaT1=deltaT;
 deltaT2=1;
 deltaM1=deltaM;
-deltaM2=0.09; 
+deltaM2=0.09;
 CL_n=2.3; % clearance rate of free virus by the immune system
 CL_in=23;
 
 p_viral=[gammaT, gammaM, deltaT, deltaM, deltaPICT, deltaPICM, kT, kM, NhatT,...
     NhatM, NT,NM,deltaT1,deltaT2,deltaM1,deltaM2,CL_n,CL_in]';
 %% Run Simulation
-TimeLen = 240;
+dose_set = [75, 150, 300, 600];
+TimeLen = 15*24;
 OutputVar = 1:15;
-[AUC,Balance,t,y] = Tenofovir(p,p_viral,OutputVar,TimeLen);
-
+y0_viral = set_initial_conditions(400);
+for i = 1:4
+    dose = dose_set(i)*10^6; %dose in ng, taken orally
+    p(1) = dose;
+    [AUC,Balance,t,y] = Tenofovir(p,p_viral,y0_viral,OutputVar,TimeLen);
+    yset{i} = y;
+    tset{i} = t;
+    balanceset{i} = Balance;
+end
+y1 = yset{1};
+y2 = yset{2};
+y3 = yset{3};
+y4 = yset{4};
 %% Plot Figures
 figure;
 ax1=subplot(2,2,1);
-plot(ax1,t,y(:,1)/(V1*10^3),'linewidth',3)
+plot(ax1,tset{1},y1(:,1)/(V1*10^3),'k',tset{2},y2(:,1)/(V1*10^3),'r',tset{3},y3(:,1)/(V1*10^3),'b',tset{4},y4(:,1)/(V1*10^3),'g','linewidth',3)
 title(ax1,'Concentration of TFV in Central Compartment')
 ylabel(ax1,'TFV (nmol/mL)')
-xlabel(ax1,'time (hrs)')
-
+xlabel(ax1,'time (hrs)');
 ax4=subplot(2,2,2);
-plot(ax4,t,y(:,4)/(Vcell2*10^3),'linewidth',3)
+plot(ax4,tset{1},y1(:,4)/(Vcell2*10^3),'k',tset{2},y2(:,4)/(Vcell2*10^3),'r',tset{3},y3(:,4)/(Vcell2*10^3),'b',tset{4},y4(:,4)/(Vcell2*10^3),'g','linewidth',3)
 title(ax4,'Concentration of TFV-MP in PBMC') %(zero = balance)
 ylabel(ax4,'TFV-MP (nmol/mL)')
 xlabel(ax4,'time (hrs)')
 
 ax4=subplot(2,2,3);
-plot(ax4,t,y(:,5)/(Vcell2*10^3),'linewidth',3)
+plot(ax4,tset{1},y1(:,5)/(Vcell2*10^3),'k',tset{2},y2(:,5)/(Vcell2*10^3),'r',tset{3},y3(:,5)/(Vcell2*10^3),'b',tset{4},y4(:,5)/(Vcell2*10^3),'g','linewidth',3)
 title(ax4,'Concentration of TFV-DP in PBMC') %(zero = balance)
 ylabel(ax4,'TFV-DP (nmol/mL)')
 xlabel(ax4,'time (hrs)')
 
 ax4=subplot(2,2,4);
-plot(ax4,t,Balance,'k','linewidth',3)
+plot(ax4,tset{1},balanceset{1},'k',tset{2},balanceset{2},'r',tset{3},balanceset{3},'b',tset{4},balanceset{4},'g','linewidth',3)
 title(ax4,'Molecular Balance') %(zero = balance)
 ylabel(ax4,'Balance of Drug (nmol)')
 xlabel(ax4,'time (hrs)')
-
-figure;
-ax1=subplot(1,3,1);
-plot(ax1,t,y(:,12),'linewidth',3)
-title(ax1,'Number of infected T-cells after proviral genomic integration')
-ylabel(ax1,'T2')
-xlabel(ax1,'time (hrs)')
-
-
-ax4=subplot(1,3,2);
-plot(ax4,t,y(:,13),'linewidth',3)
-title(ax4,'Number of infected macrophages after proviral genomic integration')
-ylabel(ax4,'M2')
-xlabel(ax4,'time (hrs)')
-
-ax4=subplot(1,3,3);
-plot(ax4,t,y(:,14)+y(:,15),'linewidth',3)
-title(ax4,'Viral Load') %(zero = balance)
-ylabel(ax4,'VI + VNI')
-xlabel(ax4,'time (hrs)')
+legend('75 mg','150 mg','300 mg','600 mg');
+% figure;
+% ax1=subplot(1,3,1);
+% plot(ax1,t,log10(y(:,12)),'linewidth',3)
+% title(ax1,'Number of infected T-cells after proviral genomic integration')
+% ylabel(ax1,'log10 T2')
+% xlabel(ax1,'time (hrs)')
+% 
+% 
+% ax4=subplot(1,3,2);
+% plot(ax4,t,log10(y(:,13)),'linewidth',3)
+% title(ax4,'Number of infected macrophages after proviral genomic integration')
+% ylabel(ax4,'log10 M2')
+% xlabel(ax4,'time (hrs)')
+% 
+% ax4=subplot(1,3,3);
+% plot(ax4,t,log10(-(y(:,14)+y(:,15) - (y0_viral(7)+y0_viral(8)))),'linewidth',3)
+% title(ax4,'Viral Load Decay')
+% ylabel(ax4,'Log10 Viral Load Decay')
+% xlabel(ax4,'time (hrs)')
