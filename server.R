a4ebc3a02875b636df8ec7447e4cc5468ae09f3b
hhuang57 <hhuang57@jhu.edu>
2016-05-12 01:10:06 -0400

    shiny test

--- server.R
+++ server.R
@@ -4,7 +4,10 @@ library(R.matlab)
 library(gridExtra)
 library(ggplot2)
 library(reshape)
-
+library(shiny)
+library(plotly)
+library(reshape2)
+library(plyr)
 
 Data <- readMat("data/diffdose.mat", header=T) # Loading in simulated data
 df <- as.data.frame(Data) # convert to dataframe
@@ -22,6 +25,21 @@ S4 = subset(df,select=c(time.4,Dose.4))
 S4 <- rename(S4,c('time.4' = 'time'))
 S4 <- melt(S4,id.vars='time')
 
+# Import data.
+Data_stats <- readMat("data/mu_sd.mat",header=T) 
+df_stats <- as.data.frame(Data_stats) 
+Data_subj <- readMat("data/subject.mat",header=T) 
+df_subj <- as.data.frame(Data_subj)
+
+#rename subpoulation
+df_subj <-rename(df_subj,c('subject.1'='Male Age 12 1970','subject.7'='Male Age 12 2000','subject.4'='Female Age 12 1970',
+                           'subject.10'='Female Age 12 2000','subject.2'='Male Age 20 1970','subject.8'='Male Age 20 2000',
+                           'subject.5'='Female Age 20 1970','subject.11'='Female Age 20 2000','subject.3'='Male Age 50 1970',
+                           'subject.9'='Male Age 50 2000','subject.6'='Female Age 50 1970','subject.12'='Female Age 50 2000'))
+
+df_melt <- melt(df_subj,id.vars="subjectid") 
+
+
 org_plot <- ggplot(NULL) + # Base plot: only experimental data, no simulated data
   geom_line(data=df,aes(x=time.1,y=Dose.1),color='red', size=0.5,alpha=0.3) +
   geom_line(data=df,aes(x=time.2,y=Dose.2),color='blue',size=0.5,alpha=0.3) +
@@ -36,8 +54,29 @@ org_plot <- ggplot(NULL) + # Base plot: only experimental data, no simulated dat
 
 
 shinyServer(function(input, output) {
+  
+  output$plot1 <- renderPlotly({ 
+    
+    #base plot for fig 1
+    box <- ggplot(df_melt,aes(x=variable,y=value,fill='grey'),alpha=0.2) + 
+      geom_boxplot() +
+      scale_x_discrete(breaks=NULL) +
+      
+      #limit y range based on slider input
+      scale_y_continuous("Weight (lbs)",limits=input$slider) +
+      ggtitle("Weight Distribution") + 
+      guides(fill=FALSE) 
+    
+    #highlight specified subpopulation
+    addd <- df_melt[df_melt$variable %in% input$Comp,]
+    boxx<-box
+    boxx <- boxx +
+      geom_boxplot(data = addd,aes(x=variable,y=value),fill='blue',alpha=0.6)
+    p1 <- ggplotly(boxx)
+    
+  })
 
-  output$plot <- renderPlotly({ # plot Figure 2: Optimized Patient Simulations
+  output$plot2 <- renderPlotly({ # plot Figure 2: Optimized Patient Simulations
     dataInput_line <- switch(input$Dose, # Select simulation data subset by highlighted Dose
                              "Dose 1" = S1, "Dose 2" = S2, "Dose 3" = S3,
                              "Dose 4" = S4)
@@ -50,7 +89,11 @@ shinyServer(function(input, output) {
     p <- ggplotly(gg)
   })
   
-  output$text <- renderText({ # Figure 2 caption
+  output$text1 <- renderText({ # Figure 2 caption
+    paste("Dose 1=75 mg, Dose 2=150 mg, Dose 3=300 mg, Dose 4=600 mg. You are currently highlighting data in red for ", input$Dose)
+  })
+  
+  output$text2 <- renderText({ # Figure 2 caption
     paste("Dose 1=75 mg, Dose 2=150 mg, Dose 3=300 mg, Dose 4=600 mg. You are currently highlighting data in red for ", input$Dose)
   })
 }) 
--- ui.R
+++ ui.R
@@ -3,21 +3,42 @@ library(plotly)
 
 shinyUI(fluidPage(
   titlePanel("Data Visualization: Missed Dose Analysis"),
-  
+  fluidRow(
+    
+    column(12,
+           fluidRow(
+             column(6,
+                    plotlyOutput("plot1",700,400),
+                    textOutput("text1")),
+             column(5,
+                    plotlyOutput("plot2",600,400),
+                    textOutput("text2")
+             )
+           )
+    )),
+  #slider widget to zoom
+  fluidRow(
+    column(10,
+           sliderInput("slider", label = h3("adjust the weight scale to zoom"), min = 0, 
+                       max = 1000, value = c(0, 400))
+    )
+  ),
   wellPanel(
     selectInput("Dose", 
                 "Select Dose data to highlight",
                 choices = c("Dose 1", "Dose 2", "Dose 3", "Dose 4"),
                 selected = "Dose 1"),
 
-    br()
-    
-    
-  ),
-  fluidRow(
-    column(8,
-           plotlyOutput("plot",600,400),
-           textOutput("text")
-    )
+    br(),
+    selectInput("Comp","Choose Subpopulation of Interest:",
+                choices = list("Male Age 12 1970","Male Age 12 2000","Female Age 12 1970",
+                               "Female Age 12 2000","Male Age 20 1970", "Male Age 20 2000", 
+                               "Female Age 12 1970","Female Age 20 1970","Male Age 50 1970",
+                               "Male Age 50 2000","Female Age 50 1970","Female Age 50 2000"),selected="Male Age 12 1970") 
   )
-))
+    
+  )
+
+
+
+)
